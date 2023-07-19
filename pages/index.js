9f2a990513457a763fb84f46e4313f3bd8a75213
Felipe Rudek Schner <frudek@Felipes-MacBook-Air.local>
2023-07-19 17:07:19 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <div>Home</div>
+    return <div>
+        <h1>Teste!</h1>
+        <h2>Teste Git</h2>
+        <h3>Teste Vercel</h3></div>
+    
 }
 
 export default Home
